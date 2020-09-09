d0a8fe1f805b8ace840980e0a0d20187a9957274
Franklin Tche <franklintche@yahoo.fr>
2020-09-09 13:49:20 +0100

    add files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name strapiAngularFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the strapiAngularFrontApp
+ */
+angular.module('strapiAngularFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name strapiAngularFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the strapiAngularFrontApp
+ */
+angular.module('strapiAngularFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
